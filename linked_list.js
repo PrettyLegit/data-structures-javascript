10fb96e94a8fd75e926f97e37d3ba122f8e697ae
Jimmy Nguyen <55465528+PrettyLegit@users.noreply.github.com>
2024-04-24 23:51:27 -0400

    Create linked_list.js

---
+++ linked_list.js
@@ -0,0 +1,56 @@
+class Node {
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(val){
+        let newNode = new Node(val); // {val: goodbye, next: null}
+        if(!this.head){ // hello 
+            this.head = newNode;
+            this.tail = this.head;
+        }
+        else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    pop(){
+        if(!this.head){
+            return null;
+        }
+        let current = this.head;
+        let newTail = current;
+        while(current.next){
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if(this.length === 0){
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
+}
+
+let list = new SinglyLinkedList();
+list.push("Hello");
+list.push("Goodbye");
+list.pop();
+list.pop();
+
+console.log(list);
