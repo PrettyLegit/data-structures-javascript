2a7e3a7e080b87fb86717557f9d6f76b5874f203
Jimmy Nguyen <55465528+PrettyLegit@users.noreply.github.com>
2024-04-25 01:04:54 -0400

    code format and created unshift method

--- linked_list.js
+++ linked_list.js
@@ -1,71 +1,82 @@
 class Node {
-    constructor(val){
-        this.val = val;
-        this.next = null;
-    }
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
 }
 
 class SinglyLinkedList {
-    constructor(){
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
-    }
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
 
-    push(val){
-        let newNode = new Node(val); // {val: goodbye, next: null}
-        if(!this.head){ // hello 
-            this.head = newNode;
-            this.tail = this.head;
-        }
-        else {
-            this.tail.next = newNode;
-            this.tail = newNode;
-        }
-        this.length++;
-        return this;
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
+    this.length++;
+    return this;
+  }
 
-    pop(){
-        if(!this.head){
-            return undefined;
-        }
-        let current = this.head;
-        let newTail = current;
-        while(current.next){
-            newTail = current;
-            current = current.next;
-        }
-        this.tail = newTail;
-        this.tail.next = null;
-        this.length--;
-        if(this.length === 0){
-            this.head = null;
-            this.tail = null;
-        }
-        return current;
+  pop() {
+    if (!this.head) {
+      return undefined;
     }
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
 
-    shift(){
-        if(!this.head){
-            return undefined;
-        }
-        let currHead = this.head;
-        this.head = currHead.next;
-        this.length--;
-        if(this.length === 0){
-            this.head = null;
-            this.tail = null;
-        }
-        return currHead;
+  shift() {
+    if (!this.head) {
+      return undefined;
     }
+    let currHead = this.head;
+    this.head = currHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return currHead;
+  }
+
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+
+    return this;
+  }
 }
 
 let list = new SinglyLinkedList();
-list.push("Hello");
-list.push("Goodbye");
-list.push("!");
-console.log(list.shift())
-console.log(list.shift())
-console.log(list.shift())
+// list.push("Hello");
+// list.push("Goodbye");
+// list.push("!");
+list.unshift("test");
 console.log(list);
